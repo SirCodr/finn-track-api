77a5d6537d073b3dbe4265fc8a66a0c1c4d5c538
SirCodr <jotadeveloper00@gmail.com>
2024-08-31 20:39:52 -0500

    Add currency pair type

--- src/symbols/types.d.ts
+++ src/symbols/types.d.ts
@@ -1,7 +1,15 @@
-export type SymbolHistoryFetchOptions = {
+export type SymbolHistoryFetchParams = {
   startTimestamp: number
   endTimestamp: number
   interval: FetchStockPeriodOptions
 }
 
 export type FetchStockPeriodOptions = '1m' | '2m' | '5m' | '15m' | '30m' | '60m' | '90m' | '1h' | '1d' | '5d' | '1wk' | '1mo' | '3mo'
+
+export type CurrencyPairFetchParams = {
+  fromCurrency: string
+  toCurrency: string
+  startDate: string
+  endDate: string
+  interval: string
+}
