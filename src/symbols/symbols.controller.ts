6506aec78e721d3dd1f1273cad215b9e684294d2
SirCodr <jotadeveloper00@gmail.com>
2024-08-31 20:39:43 -0500

    Refactor symbols

--- src/symbols/symbols.controller.ts
+++ src/symbols/symbols.controller.ts
@@ -1,19 +1,15 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { StockApiService } from 'src/api/api.service';
 import { SymbolHistoryFetchOptionsDto } from './dto/symbol.dto';
+import { SymbolsService } from './symbols.service';
 
 @Controller('symbols')
 export class SymbolsController {
-  constructor(private stockApi: StockApiService){}
+  constructor(private symbolsService: SymbolsService){}
 
   @Get(':symbol')
   async findSymbolHistory(@Param('symbol') symbol: string, @Query() options: SymbolHistoryFetchOptionsDto) {
-  const { interval, startTimestamp, endTimestamp } = options
-  const response = await this.stockApi.get(`chart/${symbol}`, {
-    interval,
-    period1: startTimestamp,
-    period2: endTimestamp
-  })
+  const response = await this.symbolsService.findSymbolHistory(symbol, options)
 
   return response.data
   }
--- src/symbols/symbols.service.ts
+++ src/symbols/symbols.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { StockApiService } from 'src/api/api.service'
+import { SymbolHistoryFetchParams } from './types'
 
 @Injectable()
 export class SymbolsService {
-  findSymbolHistory(symbol: string) {
+  constructor(private stockApi: StockApiService) {}
 
+  async findSymbolHistory(symbol: string, options: SymbolHistoryFetchParams) {
+    const { interval, startTimestamp, endTimestamp } = options
+    return await this.stockApi.get(`chart/${symbol}`, {
+      interval,
+      period1: startTimestamp,
+      period2: endTimestamp,
+    })
   }
 }
